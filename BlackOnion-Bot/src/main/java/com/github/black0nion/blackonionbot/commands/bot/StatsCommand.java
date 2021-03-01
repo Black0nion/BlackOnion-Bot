43341ac24fd93650bf7251dfdf66471aaded02a1
SIMULATAN <simulatan2319@gmail.com>
2021-03-01 12:05:05 +0100

    Added uptime stat

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/bot/StatsCommand.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/bot/StatsCommand.java
@@ -12,6 +12,7 @@ import com.github.black0nion.blackonionbot.bot.BotInformation;
 import com.github.black0nion.blackonionbot.bot.CommandBase;
 import com.github.black0nion.blackonionbot.commands.Command;
 import com.github.black0nion.blackonionbot.enums.Category;
+import com.github.black0nion.blackonionbot.utils.EmbedUtils;
 import com.github.black0nion.blackonionbot.utils.ValueManager;
 
 import net.dv8tion.jda.api.EmbedBuilder;
@@ -27,8 +28,13 @@ public class StatsCommand implements Command {
 	@Override
 	public void execute(String[] args, MessageReceivedEvent e, Message message, Member member, User author, Guild guild, MessageChannel channel) {
 		try {
-	        
-			EmbedBuilder builder = new EmbedBuilder()
+	        long diff = System.currentTimeMillis() - Bot.startTime;
+			long diffSeconds = diff / 1000 % 60;
+            long diffMinutes = diff / (60 * 1000) % 60;
+            long diffHours = diff / (60 * 60 * 1000) % 24;
+            long diffDays = diff / (24 * 60 * 60 * 1000);
+			
+			EmbedBuilder builder = EmbedUtils.getSuccessEmbed(author, guild)
 				.setTitle("Bot Stats")
 				.setFooter(author.getName() + author.getDiscriminator(), author.getEffectiveAvatarUrl())
 				.addField("Prefix", BotInformation.getPrefix(guild), true)
@@ -43,6 +49,7 @@ public class StatsCommand implements Command {
 				.addField("Messages sent", String.valueOf(ValueManager.getInt("messagesSent")), true)
 				.addField("Commands", String.valueOf(CommandBase.commands.size()), true)
 				.addField("Ping", e.getJDA().getGatewayPing() + "ms", true)
+				.addField("Uptime", (diffDays != 0 ? diffDays + " days" : "") + (diffHours != 0 ? " " + diffHours + " hours" : "") + (diffMinutes != 0 ? " " + diffMinutes + " minutes" : "") + (diffSeconds != 0 ? " " + diffSeconds + " seconds" : ""), false)
 				.setThumbnail("https://image.sv-studios.net/15d06c22eb6b0b8dfbdeda94a56c878d15.png")
 				.setTimestamp(Instant.now());
 			channel.sendMessage(builder.build()).queue();
