ab0f02b3f7fe2c551b82252fd01a1cd54ec8d7f5
SIMULATAN <simulatan2319@gmail.com>
2021-04-02 13:18:48 +0200

    Small QOL changes

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/bot/StatsCommand.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/bot/StatsCommand.java
@@ -37,7 +37,7 @@ public class StatsCommand implements Command {
 			EmbedBuilder builder = EmbedUtils.getSuccessEmbed(author, guild)
 				.setTitle("Bot Stats")
 				.setFooter(author.getName() + author.getDiscriminator(), author.getEffectiveAvatarUrl())
-				.addField("Prefix", BotInformation.getPrefix(guild), true)
+				.addField("Prefix", "``" + BotInformation.getPrefix(guild) + "``", true)
 				.addField("RunMode", Bot.runMode.name().toUpperCase(), true)
 				.addField("OS", osName, true)
 				.addField("CPU Name", cpuName, true)
