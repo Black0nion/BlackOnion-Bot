e207f17e4586c58fe4f25011fa29aa39e6eb94e6
SIMULATAN <simulatan2319@gmail.com>
2021-05-10 19:31:27 +0200

    Small changes to UpdateValue WebSocket (see desc)
    - log heartbeats when specified in the config
    - change delay to 1 Minute
    - add function to send a message to a session

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/API/impl/UpdateValue.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/API/impl/UpdateValue.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
-import org.eclipse.jetty.websocket.api.CloseStatus;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
@@ -18,9 +17,13 @@ import com.github.black0nion.blackonionbot.Logger;
 import com.github.black0nion.blackonionbot.API.WebSocketEndpoint;
 import com.github.black0nion.blackonionbot.bot.Bot;
 import com.github.black0nion.blackonionbot.misc.LogOrigin;
+import com.github.black0nion.blackonionbot.utils.ValueManager;
 
 @WebSocket
 public class UpdateValue implements WebSocketEndpoint {
+	
+	private static boolean logHeartbeats = ValueManager.getBoolean("logHeartbeats");
+	
 	private static final List<Session> sessions = new ArrayList<>();
 	
 	private static final HashMap<Session, ScheduledFuture<?>> futures = new HashMap<>();
@@ -32,7 +35,7 @@ public class UpdateValue implements WebSocketEndpoint {
 		futures.put(session, Bot.scheduledExecutor.schedule(() -> {
 			Logger.logInfo("IP " + session.getRemote().getInetSocketAddress().getAddress().getHostAddress() + " Timed Out.", LogOrigin.DASHBOARD);
 			session.close(4408, "Missed Heartbeat");
-		}, 1, TimeUnit.SECONDS));
+		}, 1, TimeUnit.MINUTES));
 	}
 	
 	@OnWebSocketClose
@@ -43,17 +46,22 @@ public class UpdateValue implements WebSocketEndpoint {
 	
 	@OnWebSocketMessage
 	public void message(Session session, String message) {
-		Logger.logInfo("IP " + session.getRemote().getInetSocketAddress().getAddress().getHostAddress() + " Received: " + message.replace("\n", "\\n"), LogOrigin.DASHBOARD);
 		if (message.equals("heartbeat")) {
-			Logger.logInfo("IP " + session.getRemote().getInetSocketAddress().getAddress().getHostAddress() + " Heartbeat.", LogOrigin.DASHBOARD);
 			futures.get(session).cancel(true);
+			if (logHeartbeats) Logger.logInfo("IP " + session.getRemote().getInetSocketAddress().getAddress().getHostAddress() + " Heartbeat.", LogOrigin.DASHBOARD);
+			send(session, "heartbeat");
 			futures.put(session, Bot.scheduledExecutor.schedule(() -> {
 				Logger.logInfo("IP " + session.getRemote().getInetSocketAddress().getAddress().getHostAddress() + " Timed Out.", LogOrigin.DASHBOARD);
 				session.close(4408, "Missed Heartbeat");
-			}, 1, TimeUnit.SECONDS));
+			}, 1, TimeUnit.MINUTES));
+			return;
 		}
+		Logger.logInfo("IP " + session.getRemote().getInetSocketAddress().getAddress().getHostAddress() + " Received: " + message.replace("\n", "\\n"), LogOrigin.DASHBOARD);
+	}
+	
+	private static final void send(Session session, String message) {
 		try {
-			session.getRemote().sendString("Hi :)");
+			session.getRemote().sendString(message);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
