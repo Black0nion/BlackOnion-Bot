e3c82225769267f6e81dfeabe4b98c45845a0499
_SIM_ <simugamz1@gmail.com>
2021-05-13 10:58:18 +0200

    Add multi-page system for CatBreedsCommand

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/fun/CatBreedsCommand.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/fun/CatBreedsCommand.java
@@ -1,10 +1,14 @@
 package com.github.black0nion.blackonionbot.commands.fun;
 
+import java.util.HashMap;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import com.github.black0nion.blackonionbot.bot.CommandBase;
 import com.github.black0nion.blackonionbot.commands.Command;
 import com.github.black0nion.blackonionbot.misc.Category;
+import com.github.black0nion.blackonionbot.systems.music.PlayerManager;
 import com.github.black0nion.blackonionbot.utils.EmbedUtils;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
@@ -16,6 +20,7 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
 
 public class CatBreedsCommand implements Command {
 
@@ -27,22 +32,62 @@ public class CatBreedsCommand implements Command {
 	@Override
 	public void execute(String[] args, GuildMessageReceivedEvent e, Message message, Member member, User author, Guild guild, MessageChannel channel) {
 		try {
-		Unirest.setTimeouts(0, 0);
-		HttpResponse<String> response = Unirest.get("https://api.thecatapi.com/v1/breeds?limit=10&page=0")
-		  .header("Content-Type", "application/json")
-		  .asString();
-		final JSONArray responseAsJSONArray = new JSONArray(response.getBody());
-		EmbedBuilder builder = EmbedUtils.getSuccessEmbed(author, guild).setTitle("UwU");
-		for (int i = 0; i < responseAsJSONArray.length(); i++) {
-			final JSONObject responseAsJSON = responseAsJSONArray.getJSONObject(i);
-			builder.addField(responseAsJSON.getString("name"), responseAsJSON.getString("id"), false);
-		}
-		channel.sendMessage(builder.build()).queue();
+			Unirest.setTimeouts(0, 0);
+			HttpResponse<String> response = Unirest.get("https://api.thecatapi.com/v1/breeds")
+			  .header("Content-Type", "application/json")
+			  .asString();
+			final JSONArray responseAsJSONArray = new JSONArray(response.getBody());
+			final HashMap<Integer, HashMap<String, String>> pages = new HashMap<>();
+			
+			final int pageCount = (int) Math.ceil((double) responseAsJSONArray.length() / 10D);
+			for (int i = 0; i < pageCount; i++) {
+				HashMap<String, String> entrysOnThisPage = new HashMap<>();
+				for (int j = i * 10; j < (i+1) * 10; j++) {
+					if (j >= responseAsJSONArray.length()) break;
+					final JSONObject jsonObjectForThisPage = responseAsJSONArray.getJSONObject(j);
+					entrysOnThisPage.put(jsonObjectForThisPage.getString("name"), jsonObjectForThisPage.getString("id"));
+				}
+				pages.put(i, entrysOnThisPage);
+			}
+			
+			System.out.println(pages);
+			
+			EmbedBuilder builder = EmbedUtils.getSuccessEmbed(author, guild).setTitle("UwU");
+			pages.get(0).forEach((name, value) -> {
+				builder.addField(name, value, false);
+			});
+			
+			channel.sendMessage(builder.build()).queue((msg) -> {
+				for (int i = 0; i < pages.size(); i++)
+					msg.addReaction(PlayerManager.numbersUnicode.get(i)).queue();
+				waitForPageSwitch(msg, author, pages);
+			});
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			channel.sendMessage(EmbedUtils.getErrorEmbed(author, guild).addField("errorhappened", "somethingwentwrong", false).build()).queue();
 			return;
 		}
+	}
+	
+	private static void waitForPageSwitch(Message msg, User user, HashMap<Integer, HashMap<String, String>> pages) {
+		CommandBase.waiter.waitForEvent(GuildMessageReactionAddEvent.class,
+				(event) -> event.getUserIdLong() == user.getIdLong() && event.getMessageIdLong() == msg.getIdLong(),
+				(event) -> {
+					event.getReaction().removeReaction(event.getUser()).queue();
+					Integer emojiReactionNum = PlayerManager.numbersUnicode.entrySet().stream().filter((entry) -> {return entry.getValue().equals(event.getReactionEmote().getAsCodepoints());}).findFirst().get().getKey();
+					
+					if (!event.getReactionEmote().isEmoji() || !PlayerManager.numbersUnicode.containsValue(event.getReactionEmote().getAsCodepoints()) || pages.size() < emojiReactionNum)
+						waitForPageSwitch(msg, user, pages);
+					
+					// all fine, we can switch page
+					EmbedBuilder builder = EmbedUtils.getSuccessEmbed(user, msg.getGuild()).setTitle("UwU");
+					pages.get(emojiReactionNum).forEach((name, value) -> {
+						builder.addField(name, value, false);
+					});
+					
+					msg.editMessage(builder.build()).queue();
+					waitForPageSwitch(msg, user, pages);
+				});
 	}
 
 	@Override
