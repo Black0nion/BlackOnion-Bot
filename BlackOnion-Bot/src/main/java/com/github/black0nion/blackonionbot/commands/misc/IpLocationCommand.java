ad6a62c5d49e07f9d1e68ef653e1c5b5ad068531
_SIM_ <simugamz1@gmail.com>
2021-05-18 11:18:46 +0200

    Add IpLocation Command (see desc.)
    Also added:
    - commons-validator:commons-validator dependency (to check if the IP is valid)
    - setTitle override for setTitle without URL
    - getWrongArgument utils method

---
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/misc/IpLocationCommand.java
@@ -0,0 +1,68 @@
+package com.github.black0nion.blackonionbot.commands.misc;
+
+import org.apache.commons.validator.routines.InetAddressValidator;
+import org.json.JSONObject;
+
+import com.github.black0nion.blackonionbot.commands.Command;
+import com.github.black0nion.blackonionbot.misc.Category;
+import com.github.black0nion.blackonionbot.utils.EmbedUtils;
+import com.github.black0nion.blackonionbot.utils.Utils;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+public class IpLocationCommand implements Command {
+	private static final InetAddressValidator validator = InetAddressValidator.getInstance();
+	
+	@Override
+	public String[] getCommand() {
+		return new String[] { "iplocation", "ip" };
+	}
+
+	@Override
+	public void execute(String[] args, GuildMessageReceivedEvent e, Message message, Member member, User author, Guild guild, MessageChannel channel) {
+		try {
+			if (!validator.isValid(args[1])) {
+				channel.sendMessage(EmbedUtils.getErrorEmbed(author, guild).addField("notavalidip", "pleaseentervalidip", false).build()).queue();
+				return;
+			}
+			Unirest.setTimeouts(0, 0);
+			HttpResponse<String> response = Unirest.get("https://ipapi.co/" + args[1] + "/json/").asString();
+			JSONObject object = new JSONObject(response.getBody());
+			if (object.getBoolean("error")) {
+				if (object.getBoolean("reserved")) {
+					channel.sendMessage(EmbedUtils.getErrorEmbed(author, guild).addField("ipnotpublic", "pleaseentervalidip", false).build()).queue();
+					return;
+				} else {					
+					channel.sendMessage(Utils.getWrongArgument(author, guild, this)).queue();
+					return;
+				}
+			}
+			channel.sendMessage(EmbedUtils.getSuccessEmbed(author, guild).setTitle("IP Geolocation", "https://ipapi.co").addField(object.getString("city") + ", " + object.getString("region") + " (" + object.getString("region_code") + ", " + object.getString("country") + ")", object.getString("timezone"), false).build()).queue();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			channel.sendMessage(EmbedUtils.getErrorEmbed(author, guild).addField("errorhappened", "somethingwentwrong", false).build()).queue();
+		}
+	}
+
+	@Override
+	public Category getCategory() {
+		return Category.MISC;
+	}
+	
+	@Override
+	public int getRequiredArgumentCount() {
+		return 1;
+	}
+
+	@Override
+	public String getSyntax() {
+		return "<public ipv4 / ipv6>";
+	}
+}
