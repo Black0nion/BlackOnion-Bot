ea55d8a8ef60fa03c3d9188162063bb3dc879d6b
_SIM_ <simugamz1@gmail.com>
2021-05-25 14:37:48 +0200

    BB = Big Bruh

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/bot/AntiSwearCommand.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/bot/AntiSwearCommand.java
@@ -25,9 +25,12 @@ public class AntiSwearCommand implements Command {
 	@Override
 	public void execute(String[] args, GuildMessageReceivedEvent e, BlackMessage message, BlackMember member, BlackUser author, BlackGuild guild, TextChannel channel) {
 		
-		if (!guild.isPremium()) channel.sendMessage(EmbedUtils.premiumRequired(author, guild));
+		if (!guild.isPremium()) {
+			channel.sendMessage(EmbedUtils.premiumRequired(author, guild)).queue();
+			return;
+		}
 		
-		if (Utils.handleRights(guild, author, null, Permission.MESSAGE_MANAGE)) return;
+		if (Utils.handleRights(guild, author, channel, Permission.MESSAGE_MANAGE)) return;
 		if (args.length >= 2) {
 			final String type = args[1];
 			if (type.equalsIgnoreCase("remove")) {
