4ff0a1c096fe9ea3f3655361229c4f78d49398a2
_SIM_ <simugamz1@gmail.com>
2021-05-28 10:03:47 +0200

    Improve logging

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/Logger.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/Logger.java
@@ -1,38 +1,37 @@
 package com.github.black0nion.blackonionbot;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import com.github.black0nion.blackonionbot.misc.LogMode;
 import com.github.black0nion.blackonionbot.misc.LogOrigin;
+import com.google.common.io.FileWriteMode;
+import com.google.common.io.Files;
 
 public class Logger {
 	
-	private static final SimpleDateFormat dtf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss ");
+	private static final SimpleDateFormat dtf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 	
 	public static void log(LogMode mode, LogOrigin origin, String logInput) {
 		//StackTraceElement stackTraceElement = Thread.currentThread().getStackTrace()[Thread.currentThread().getStackTrace().length - 1];
 		//String log = dtf.format(now) + "[" + stackTraceElement.getFileName().replace(".java", "") + "." + stackTraceElement.getMethodName() + ":" + stackTraceElement.getLineNumber() + "] [" + mode.name() + "] " + logInput;
-		String log = dtf.format(new Date()) + "[" + origin.name() + "] [" + mode.name() + "] " + logInput;
+		final String log = dtf.format(new Date()) + " [" + origin.name() + "] [" + mode.name() + "] " + logInput;
 		if (mode == LogMode.ERROR || mode == LogMode.FATAL) {
 			System.err.println(log);
+		} else if (mode == LogMode.WARNING) {
+			System.out.println("\033[33m" + log + "\033[0m");
 		} else {
-			System.out.println(log);
+			System.out.println("\033[94m" + log + "\033[0m");
 		}
+		
 		try {
-			File file = new File("files/logs/log." + origin.name().toLowerCase() + "/" + origin.name().toLowerCase() + "." + mode.name().toLowerCase() + ".log");
+			final File file = new File("files/logs/log." + origin.name().toLowerCase() + "/" + origin.name().toLowerCase() + "." + mode.name().toLowerCase() + ".log");
 			file.getParentFile().mkdirs();
 			file.createNewFile();
-			PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("files/logs/log." + origin.name().toLowerCase() + "/" + origin.name().toLowerCase() + "." + mode.name().toLowerCase() + ".log", true)));
-		    out.println(log);
-		    out.close();
-		    PrintWriter out2 = new PrintWriter(new BufferedWriter(new FileWriter("files/logs/log", true)));
-		    out2.println(log);
-		    out2.close();
+			Files.asCharSink(file, StandardCharsets.UTF_8, FileWriteMode.APPEND).write(log);
+			Files.asCharSink(new File("files/logs/bot.log"), StandardCharsets.UTF_8, FileWriteMode.APPEND).write(log);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
