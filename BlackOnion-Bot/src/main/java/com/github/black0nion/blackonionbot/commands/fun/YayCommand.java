bde5e32f9c4e78317c4179a55727caa448242837
SIMULATAN <simulatan2319@gmail.com>
2021-05-30 18:38:25 +0200

    Add yay command (totally necessary)

---
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/fun/YayCommand.java
@@ -0,0 +1,23 @@
+package com.github.black0nion.blackonionbot.commands.fun;
+
+import com.github.black0nion.blackonionbot.blackobjects.BlackGuild;
+import com.github.black0nion.blackonionbot.blackobjects.BlackMember;
+import com.github.black0nion.blackonionbot.blackobjects.BlackMessage;
+import com.github.black0nion.blackonionbot.blackobjects.BlackUser;
+import com.github.black0nion.blackonionbot.commands.Command;
+import com.github.black0nion.blackonionbot.commands.CommandEvent;
+
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+public class YayCommand extends Command {
+	
+	public YayCommand() {
+		this.setCommand("yay");
+	}
+
+	@Override
+	public void execute(String[] args, CommandEvent cmde, GuildMessageReceivedEvent e, BlackMessage message, BlackMember member, BlackUser author, BlackGuild guild, TextChannel channel) {
+		cmde.success("yay", "is workin :D");
+	}
+}
