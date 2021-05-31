72676c32cfd128b5ebf9549e49e6e948a1d78368
_SIM_ <simugamz1@gmail.com>
2021-05-31 10:24:57 +0200

    break it

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/misc/PasteCommand.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/misc/PasteCommand.java
@@ -1,5 +1,8 @@
 package com.github.black0nion.blackonionbot.commands.misc;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.json.JSONObject;
 
 import com.github.black0nion.blackonionbot.blackobjects.BlackGuild;
@@ -8,8 +11,10 @@ import com.github.black0nion.blackonionbot.blackobjects.BlackMessage;
 import com.github.black0nion.blackonionbot.blackobjects.BlackUser;
 import com.github.black0nion.blackonionbot.commands.Command;
 import com.github.black0nion.blackonionbot.commands.CommandEvent;
+import com.github.black0nion.blackonionbot.utils.Utils;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.request.HttpRequestWithBody;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.TextChannel;
@@ -19,35 +24,44 @@ public class PasteCommand extends Command {
 	
 	public PasteCommand() {
 		this.setCommand("paste", "uploadtext")
-			.setSyntax("<language> <text>")
-			.setRequiredArgumentCount(2);
+			.setSyntax("<text (codeblock with language specification if wanted)>")
+			.setRequiredArgumentCount(1);
 	}
 
 	@Override
 	public void execute(String[] args, CommandEvent cmde, GuildMessageReceivedEvent e, BlackMessage message, BlackMember member, BlackUser author, BlackGuild guild, TextChannel channel) {
-		final String language = args[1];
-		final String contentRaw = e.getMessage().getContentRaw();
-		final int index = contentRaw.indexOf(language) + language.length();
-		final String bodyLong = contentRaw.substring(index).trim();
-		// todo pls help alvin
+		final String bodyRaw = String.join(" ", Utils.removeFirstArg(args)).trim();
+		// broken lol
+		final Matcher m = Pattern.compile("\\`\\`\\`(?:([a-zA-Z]+)\\n)?(.*)\\`\\`\\`").matcher(bodyRaw);
+		String body = null, language = null;
+		
+		try {
+			language = m.group(1);
+		} catch (Exception ignored) { }
+		
+		try {
+			body = m.group(2);
+		} catch (Exception ignored) { body = bodyRaw; }
+		
+		final String finalLanguage = language;
+		final String finalBody = body;
 		
 		cmde.loading(msg -> {				
 			try {
 				Unirest.setTimeouts(0, 0);
-				HttpResponse<String> response = Unirest.post("https://paste.sv-studios.net/documents")
-						.header("Content-Type", "text/plain")
-						.header("language", language)
-						.body(bodyLong)
-						.asString();
+				final HttpRequestWithBody headers = Unirest.post("https://paste.sv-studios.net/documents").header("Content-Type", "text/plain");
+				
+				if (finalLanguage != null) headers.header("language", finalLanguage);
+				
+				HttpResponse<String> response = headers.body(finalBody).asString();
 				
 				JSONObject obj = new JSONObject(response.getBody());
 				
 				final EmbedBuilder builder = cmde.success()
 						.setTitle("pastecreated", "https://paste.sv-studios.net/" + obj.getString("key"))
-						.setDescription("```")
-						.appendDescription(language)
+						.setDescription("```" + (finalLanguage != null ? finalLanguage : ""))
 						.appendDescription("\n")
-						.appendDescription(bodyLong)
+						.appendDescription(finalBody)
 						.appendDescription("```");
 				
 				msg.editMessage(builder.build()).queue();
