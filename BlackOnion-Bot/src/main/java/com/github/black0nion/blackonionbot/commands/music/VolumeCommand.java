2f64453a68d043240f70ad2ee5d4e0e67f90b21a
_SIM_ <simugamz1@gmail.com>
2021-05-31 15:29:54 +0200

    Add VolumeCommand

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/music/VolumeCommand.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/music/VolumeCommand.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 package com.github.black0nion.blackonionbot.commands.music;
 
 import com.github.black0nion.blackonionbot.blackobjects.BlackGuild;
@@ -8,16 +11,24 @@ import com.github.black0nion.blackonionbot.commands.Command;
 import com.github.black0nion.blackonionbot.commands.CommandEvent;
 import com.github.black0nion.blackonionbot.systems.music.GuildMusicManager;
 import com.github.black0nion.blackonionbot.systems.music.PlayerManager;
+import com.github.black0nion.blackonionbot.utils.Placeholder;
+import com.github.black0nion.blackonionbot.utils.Utils;
 
 import net.dv8tion.jda.api.entities.GuildVoiceState;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
-public class StopCommand extends Command {
+/**
+ * @author _SIM_
+ *
+ */
+public class VolumeCommand extends Command {
 	
-	public StopCommand() {
-		this.setCommand("stop");
+	public VolumeCommand() {
+		this.setCommand("volume", "earrapestrength")
+			.setSyntax("<volume>")
+			.setRequiredArgumentCount(1);
 	}
 
 	@Override
@@ -27,11 +38,18 @@ public class StopCommand extends Command {
 			final VoiceChannel memberChannel = member.getVoiceState().getChannel();
 			if (memberChannel != null && memberChannel.getIdLong() == state.getChannel().getIdLong()) {				
 				final GuildMusicManager musicManager = PlayerManager.getInstance().getMusicManager(e.getChannel());
-				musicManager.scheduler.player.stopTrack();
-				musicManager.scheduler.queue.clear();
-				e.getGuild().getAudioManager().closeAudioConnection();
-				
-				cmde.success("musicstopped", "leftvc");
+				if (Utils.isLong(args[1])) {					
+					final int volume = Integer.parseInt(args[1]);
+					if (volume > 0 && volume <= 150) {						
+						musicManager.scheduler.player.setVolume(volume);
+						cmde.success("volumechanged", "volumesetto", new Placeholder("volume", volume));
+					} else {
+						cmde.error("invalidvolume", "volumerange");
+					}
+					
+				} else {
+					cmde.error("notanumber", "inputnumber");
+				}
 			} else {
 				cmde.error("notinsamevc", "dontstopotherpplmusic");
 			}
