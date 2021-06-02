312b0f10f2d5563aa2cbb5da9a52c0757170561b
_SIM_ <simugamz1@gmail.com>
2021-06-02 16:20:24 +0200

    Use GuildType instead of boolean for isPremium and more stuff

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/utils/EmbedUtils.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/utils/EmbedUtils.java
@@ -56,11 +56,12 @@ public class EmbedUtils {
 				.setTimestamp(Instant.now());
 	}
 	
+	private static final Color premiumColor = new Color(245, 189, 2);
 	public static MessageEmbed premiumRequired(BlackUser author, BlackGuild guild) {
 		EmbedBuilder builder = new Embed(author, guild)
 				.setTitle("error")
 				.addField("notpremium", "premiumrequired", false)
-				.setColor(Color.RED)
+				.setColor(premiumColor)
 				.setTimestamp(Instant.now());
 		if (author != null) builder.setFooter(author.getName() + "#" + author.getDiscriminator(), author.getEffectiveAvatarUrl());
 		return builder.build();
