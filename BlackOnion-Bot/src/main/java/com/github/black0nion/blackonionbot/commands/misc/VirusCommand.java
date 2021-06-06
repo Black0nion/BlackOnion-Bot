fd23e4c9bbda897536d03f581fa02d3d518a8ec8
SIMULATAN <simulatan2319@gmail.com>
2021-06-06 07:47:34 +0200

    Translate VirusCommand

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/misc/VirusCommand.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/misc/VirusCommand.java
@@ -11,6 +11,7 @@ import com.github.black0nion.blackonionbot.blackobjects.BlackUser;
 import com.github.black0nion.blackonionbot.bot.Bot;
 import com.github.black0nion.blackonionbot.commands.Command;
 import com.github.black0nion.blackonionbot.commands.CommandEvent;
+import com.github.black0nion.blackonionbot.utils.Placeholder;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 
@@ -43,20 +44,23 @@ public class VirusCommand extends Command {
 				cmde.sendPleaseUse();
 				return;
 			}
+			
 			final String finalUrl = url;
 			final String urlResult = checkUrl(finalUrl).getBody();
 			if (!new JSONObject(urlResult).has("data")) {
 				cmde.sendPleaseUse();
 				return;
 			}
+			
 			cmde.loading("fileprocessed", "waitforresult", msg -> {
 				JSONObject analyse = null;
+				int i = 1;
 				do  { 
-					try { Thread.sleep(5000); } catch (InterruptedException ex) { ex.printStackTrace(); }
+					try { Thread.sleep(5000 * i); i++; } catch (InterruptedException ex) { ex.printStackTrace(); }
 					analyse = new JSONObject(checkAnalyse(new JSONObject(urlResult).getJSONObject("data").getString("id")).getBody()); 
 				} while (!analyse.has("data"));
 				final JSONObject result = analyse.getJSONObject("data").getJSONObject("attributes").getJSONObject("stats");
-				msg.editMessage(cmde.success().addField("Result: " + (result.getInt("malicious") == 0 ? "No Virus." : "Virus!"), "Malicious: " + result.getInt("malicious") + "\nUndetected: " + result.getInt("undetected") + "\nSuspicious: " + result.getInt("suspicious") + "\nHarmless: " + result.getInt("harmless"), false).build()).queue();
+				msg.editMessage(cmde.success().addField(cmde.getTranslation("result", new Placeholder("result", cmde.getTranslation(result.getInt("malicious") == 0 ? "virus" : "novirus"))), cmde.getTranslation("resultlong", new Placeholder("malicious", result.getInt("malicious")), new Placeholder("sus", result.getInt("suspicious")), new Placeholder("harmless", result.getInt("harmless")), new Placeholder("undetected", result.getInt("undetected"))), false).build()).queue();
 				return;
 			});
 		});
