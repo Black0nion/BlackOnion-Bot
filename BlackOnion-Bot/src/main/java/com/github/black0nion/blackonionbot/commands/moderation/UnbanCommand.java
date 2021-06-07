2cba5f80b2c4fdf0569aed7b9a64656419792c30
_SIM_ <simugamz1@gmail.com>
2021-06-07 14:13:19 +0200

    Don't kick / ban users when they are higher than the user who tried to do that

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/moderation/UnbanCommand.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/moderation/UnbanCommand.java
@@ -16,38 +16,36 @@ import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class UnbanCommand extends Command {
-	
-	public UnbanCommand() {
-		this.setCommand("unban", "unyeet")
-			.setSyntax("<@User>")
-			.setRequiredArgumentCount(1)
-			.setRequiredPermissions(Permission.BAN_MEMBERS)
-			.setRequiredBotPermissions(Permission.BAN_MEMBERS);
+
+    public UnbanCommand() {
+	this.setCommand("unban", "unyeet").setSyntax("<@User>").setRequiredArgumentCount(1).setRequiredPermissions(Permission.BAN_MEMBERS).setRequiredBotPermissions(Permission.BAN_MEMBERS);
+    }
+
+    @Override
+    public void execute(final String[] args, final CommandEvent cmde, final GuildMessageReceivedEvent e, final BlackMessage message, final BlackMember member, final BlackUser author, final BlackGuild guild, final TextChannel channel) {
+	final List<BlackMember> mentionedMembers = message.getMentionedBlackMembers();
+	if (mentionedMembers.size() != 0) {
+	    final BlackUser bannedUser = mentionedMembers.get(0).getBlackUser();
+	    guild.retrieveBan(bannedUser).queue(ban -> {
+		cmde.success("unban", "userunbanned", "bannedfor", new Placeholder("reason", "**" + ban.getReason() + "**"));
+	    });
+	    guild.unban(bannedUser).queue();
+	} else {
+	    try {
+		if (!Utils.isLong(args[1])) {
+		    cmde.sendPleaseUse();
+		    return;
+		}
+
+		guild.retrieveBanById(args[1]).queue(ban -> {
+		    final String reason = ban.getReason();
+		    guild.unban(ban.getUser()).queue();
+		    cmde.success("unban", "userunbanned", "bannedfor", new Placeholder("reason", "**" + reason + "**"));
+		}, fail -> {
+		    cmde.error("usernotfound", "tagornameuser");
+		});
+	    } catch (final Exception ignored) {
+	    }
 	}
-	
-	@Override
-	public void execute(final String[] args, final CommandEvent cmde, final GuildMessageReceivedEvent e, final BlackMessage message, final BlackMember member, final BlackUser author, final BlackGuild guild, final TextChannel channel) {
-		final List<BlackMember> mentionedMembers = message.getMentionedBlackMembers();
-		if (mentionedMembers.size() != 0) {
-			final BlackUser bannedUser = mentionedMembers.get(0).getBlackUser();
-			guild.retrieveBan(bannedUser).queue(ban -> {
-				cmde.success("uban", "userunbanned", "bannedfor", new Placeholder("reason", "**" + ban.getReason() + "**"));
-			});
-			guild.unban(bannedUser).queue();
-		} else
-			try {
-				if (!Utils.isLong(args[1])) {
-					cmde.sendPleaseUse();
-					return;
-				}
-				
-				guild.retrieveBanById(args[1]).queue(ban -> {
-					final String reason = ban.getReason();
-					guild.unban(ban.getUser()).queue();
-					cmde.success("unban", "userunbanned", "bannedfor", new Placeholder("reason", "**" + reason + "**"));
-				}, fail -> {
-					cmde.error("usernotfound", "tagornameuser");
-				});
-			} catch (final Exception ignored) {}
-	}
+    }
 }
