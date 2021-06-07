f2b67f5f7fa6069deb6f162ed425e033abd22c22
_SIM_ <simugamz1@gmail.com>
2021-06-07 15:00:40 +0200

    Prettify WebSocketEndpoint and make the variable route private

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/API/WebSocketEndpoint.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/API/WebSocketEndpoint.java
@@ -5,22 +5,21 @@ import java.io.IOException;
 import org.eclipse.jetty.websocket.api.Session;
 
 public abstract class WebSocketEndpoint {
-	
-	String route;
-	
-	public String getRoute() {
-		return this.route;
+    private String route;
+
+    public String getRoute() {
+	return this.route;
+    }
+
+    public void setRoute(final String route) {
+	this.route = route;
+    }
+
+    public void send(final Session session, final String message) {
+	try {
+	    session.getRemote().sendString(message);
+	} catch (final IOException e) {
+	    e.printStackTrace();
 	}
-	
-	public void setRoute(String route) {
-		this.route = route;
-	}
-	
-	public void send(Session session, String message) {
-		try {
-			session.getRemote().sendString(message);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    }
 }
