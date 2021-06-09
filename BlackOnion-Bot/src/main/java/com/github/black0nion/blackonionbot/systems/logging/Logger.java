e97ab4d8c843daecac0b31c775c90fe67ec04b25
_SIM_ <simugamz1@gmail.com>
2021-06-09 15:11:53 +0200

    add option to view x latest logs in category y or log level z

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/systems/logging/Logger.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/systems/logging/Logger.java
@@ -3,12 +3,12 @@ package com.github.black0nion.blackonionbot.systems.logging;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
-import com.github.black0nion.blackonionbot.blackobjects.BlackArrayList;
+import com.github.black0nion.blackonionbot.blackobjects.BlackLinkedList;
 import com.github.black0nion.blackonionbot.misc.LogMode;
 import com.github.black0nion.blackonionbot.misc.LogOrigin;
 import com.google.common.io.FileWriteMode;
@@ -18,8 +18,9 @@ public class Logger {
 
     private static final SimpleDateFormat dtf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
-    private static final HashMap<LogOrigin, BlackArrayList<String>> logsPerCategory = new HashMap<>();
-    private static final List<String> logs = new ArrayList<>();
+    private static final HashMap<LogOrigin, BlackLinkedList<String>> logsPerCategory = new HashMap<>();
+    private static final HashMap<LogMode, BlackLinkedList<String>> logsPerLevel = new HashMap<>();
+    private static final List<String> logs = new LinkedList<>();
 
     public static void log(final LogMode mode, final LogOrigin origin, final String logInput) {
 	// StackTraceElement stackTraceElement =
@@ -43,10 +44,17 @@ public class Logger {
 	}
 
 	logs.add(consoleLog);
+
 	if (logsPerCategory.containsKey(origin)) {
 	    logsPerCategory.put(origin, logsPerCategory.get(origin).addAndGetSelf(consoleLog));
 	} else {
-	    logsPerCategory.put(origin, new BlackArrayList<String>().addAndGetSelf(consoleLog));
+	    logsPerCategory.put(origin, new BlackLinkedList<String>().addAndGetSelf(consoleLog));
+	}
+
+	if (logsPerLevel.containsKey(mode)) {
+	    logsPerLevel.put(mode, logsPerLevel.get(mode).addAndGetSelf(consoleLog));
+	} else {
+	    logsPerLevel.put(mode, new BlackLinkedList<String>().addAndGetSelf(consoleLog));
 	}
 
 	try {
@@ -92,10 +100,58 @@ public class Logger {
      * @param type
      */
     public static void printForCategory(final LogOrigin type) {
-	System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + "PEAKING: " + type.name() + "\n" + String.join("\n", logsPerCategory.get(type)));
+	if (logsPerCategory.containsKey(type)) {
+	    System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + "LOGS IN CATEGORY: " + type.name() + "\n" + String.join("\n", logsPerCategory.get(type)));
+	} else {
+	    System.out.println("No logs found for LogOrigin " + type.name());
+	}
     }
 
     public static void printAll() {
 	System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + String.join("\n", logs));
+    }
+
+    /**
+     * @param type
+     * @param length
+     */
+    public static void printForCategory(final LogOrigin type, final int length) {
+	if (logsPerCategory.containsKey(type)) {
+	    final BlackLinkedList<String> logsInCat = logsPerCategory.get(type);
+	    System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + "PEAKING: " + type.name() + "\n" + String.join("\n", logsPerCategory.get(type).subList(0, (length > logsInCat.size() ? logsInCat.size() : length))));
+	} else {
+	    System.out.println("No logs found for LogOrigin " + type.name());
+	}
+    }
+
+    /**
+     * @param length
+     */
+    public static void printAll(final int length) {
+	System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + String.join("\n", logs.subList(0, (length > logs.size() ? logs.size() : length))));
+    }
+
+    /**
+     * @param mode
+     */
+    public static void printForLevel(final LogMode mode) {
+	if (logsPerLevel.containsKey(mode)) {
+	    System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + "PEAKING: " + mode.name() + "\n" + String.join("\n", logsPerLevel.get(mode)));
+	} else {
+	    System.out.println("No logs found for LogLevel " + mode.name());
+	}
+    }
+
+    /**
+     * @param mode
+     * @param length
+     */
+    public static void printForLevel(final LogMode mode, final int length) {
+	if (logsPerLevel.containsKey(mode)) {
+	    final BlackLinkedList<String> logsInCat = logsPerLevel.get(mode);
+	    System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + "PEAKING: " + mode.name() + "\n" + String.join("\n", logsInCat.subList(0, (length > logsInCat.size() ? logsInCat.size() : length))));
+	} else {
+	    System.out.println("No logs found for LogLevel " + mode.name());
+	}
     }
 }
