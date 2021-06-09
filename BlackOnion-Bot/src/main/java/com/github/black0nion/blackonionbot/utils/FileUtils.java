5b9a4cab618da281305055052cd5d2da3392d363
_SIM_ <simugamz1@gmail.com>
2021-06-09 15:21:07 +0200

    Log messages to LogMode DISCORD, fix stupid logging mistake

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/utils/FileUtils.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/utils/FileUtils.java
@@ -9,28 +9,31 @@ import com.google.common.io.FileWriteMode;
 import com.google.common.io.Files;
 
 public class FileUtils {
-	
-	public static String readFromFile(final File file) {
-		try {
-			return String.join(" ", Files.asCharSource(file, StandardCharsets.UTF_8).readLines());
-		} catch (final IOException e) {
-			if (!(e instanceof FileNotFoundException))
-				e.printStackTrace();
-		}
-		return null;
+
+    public static String readFromFile(final File file) {
+	try {
+	    return String.join(" ", Files.asCharSource(file, StandardCharsets.UTF_8).readLines());
+	} catch (final IOException e) {
+	    if (!(e instanceof FileNotFoundException)) {
+		e.printStackTrace();
+	    }
 	}
-	
-	public static void appendToFile(final String fileName, final String input) {
-		try {
-			final File file = new File(fileName);
-			final File parentFile = file.getParentFile();
-			if (parentFile != null)
-				parentFile.mkdirs();
-			if (!file.exists())
-				file.createNewFile();
-			Files.asCharSink(file, StandardCharsets.UTF_8, FileWriteMode.APPEND);
-		} catch (final IOException e) {
-		    e.printStackTrace();
-		}
+	return null;
+    }
+
+    public static void appendToFile(final String fileName, final String input) {
+	try {
+	    final File file = new File(fileName);
+	    final File parentFile = file.getParentFile();
+	    if (parentFile != null) {
+		parentFile.mkdirs();
+	    }
+	    if (!file.exists()) {
+		file.createNewFile();
+	    }
+	    Files.asCharSink(file, StandardCharsets.UTF_8, FileWriteMode.APPEND).write(input);
+	} catch (final IOException e) {
+	    e.printStackTrace();
 	}
+    }
 }
