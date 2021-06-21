123bb2cc6e098405c846a800a8089113e5fad063
_SIM_ <simugamz1@gmail.com>
2021-06-21 18:23:52 +0200

    Remove unnecessary code in Paths API endpoint

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/API/impl/get/Paths.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/API/impl/get/Paths.java
@@ -1,8 +1,6 @@
 package com.github.black0nion.blackonionbot.API.impl.get;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import org.json.JSONArray;
@@ -20,15 +18,6 @@ public class Paths extends GetRequest {
 
     @Override
     public String handle(final Request request, final Response response, final JSONObject body, final HashMap<String, String> headers, final BlackSession user) {
-	final JSONObject obj = new JSONObject();
-
-	final List<String> routes = new ArrayList<String>();
-
-	for (final RouteMatch route : Spark.routes()) {
-	    routes.add(route.getMatchUri());
-	}
-	obj.put("paths", routes);
-
 	return new JSONArray(Spark.routes().stream().map(RouteMatch::getMatchUri).collect(Collectors.toList())).toString();
     }
 
