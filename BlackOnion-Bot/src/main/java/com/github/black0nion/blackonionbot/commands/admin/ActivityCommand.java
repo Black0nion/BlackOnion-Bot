910a4788b71c6951ead8bbaacc269c5c24f7628a
_SIM_ <simugamz1@gmail.com>
2021-07-19 19:53:46 +0200

    Add competing and streaming to ActivityCommand

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/admin/ActivityCommand.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/commands/admin/ActivityCommand.java
@@ -11,6 +11,7 @@ import com.github.black0nion.blackonionbot.utils.ValueManager;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
@@ -18,7 +19,7 @@ import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 public class ActivityCommand extends SlashCommand {
 
     public ActivityCommand() {
-	this.setData(new CommandData("activity", "Sets the activity of the bot").addOptions(new OptionData(OptionType.STRING, "type", "The type of the activity", true).addChoice("Playing", "playing").addChoice("Watching", "watching").addChoice("Listening", "listening"), new OptionData(OptionType.STRING, "message", "The custom message to stand after the type", true))).setHidden();
+	this.setData(new CommandData("activity", "Sets the activity of the bot").addOptions(new OptionData(OptionType.STRING, "type", "The type of the activity", true).addChoice("Playing", "playing").addChoice("Watching", "watching").addChoice("Listening", "listening").addChoice("Streaming", "streaming").addChoice("Competing", "competing"), new OptionData(OptionType.STRING, "message", "The custom message to stand after the type", true), new OptionData(OptionType.STRING, "url", "The URL of the stream (required for the STREAMING status)"))).setHidden();
     }
 
     @Override
@@ -45,23 +46,40 @@ public class ActivityCommand extends SlashCommand {
 	    e.getJDA().getPresence().setActivity(Activity.watching(status));
 	} else if (activityType.contains("listening")) {
 	    e.getJDA().getPresence().setActivity(Activity.listening(status));
+	} else if (activityType.contains("streaming")) {
+	    final OptionMapping option = e.getOption("url");
+	    if (option == null) {
+		cmde.errorPrivate("notwitchurl", "pleasevalidtwitchurl");
+		return;
+	    }
+	    final String url = option.getAsString();
+	    if (url.isEmpty() || !Activity.isValidStreamingUrl(url)) {
+		cmde.errorPrivate("notwitchurl", "pleasevalidtwitchurl");
+		return;
+	    }
+	    ValueManager.save("activityUrl", url);
+	    e.getJDA().getPresence().setActivity(Activity.streaming(status, url));
+	} else if (activityType.contains("competing")) {
+	    e.getJDA().getPresence().setActivity(Activity.competing(status));
 	} else {
 	    cmde.errorPrivate("wrongargument", SlashCommandExecutedEvent.getPleaseUse(guild, author, this));
 	    return;
 	}
-	cmde.successPrivate("newactivity", activityType + " " + status);
+	cmde.successPrivate("newactivity", "**" + activityType + "** " + status);
 	ValueManager.save("activityType", activityType);
 	ValueManager.save("activity", status);
     }
 
     public static Activity getActivity() {
-	return getActivity(ValueManager.getString("activityType"), ValueManager.getString("activity"));
+	return getActivity(ValueManager.getString("activityType"), ValueManager.getString("activity"), ValueManager.getString("activityUrl"));
     }
 
-    public static Activity getActivity(final String status, final String activity) {
+    public static Activity getActivity(final String status, final String activity, final String url) {
 	if (status.contains("playing")) return Activity.playing(activity);
 	else if (status.contains("watching")) return Activity.watching(activity);
 	else if (status.contains("listening")) return Activity.listening(activity);
+	else if (status.contains("streaming")) return Activity.streaming(activity, url);
+	else if (status.contains("competing")) return Activity.competing(url);
 	else return null;
     }
 }
