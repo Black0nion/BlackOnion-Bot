73d6890685cfe5dba2c2a5cae7e2929321a5939d
SIMULATAN <simulatan2319@gmail.com>
2021-07-25 14:46:56 +0200

    Switch TicTacToe to Buttons

--- BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/systems/games/tictactoe/TicTacToeGameManager.java
+++ BlackOnion-Bot/src/main/java/com/github/black0nion/blackonionbot/systems/games/tictactoe/TicTacToeGameManager.java
@@ -1,38 +1,40 @@
 package com.github.black0nion.blackonionbot.systems.games.tictactoe;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import net.dv8tion.jda.api.entities.TextChannel;
 
 public class TicTacToeGameManager {
-	private static ArrayList<TicTacToe> games = new ArrayList<>();
-	
+	private static List<TicTacToe> games = new ArrayList<>();
+
 	public static final int SIZE = 3;
-	
+
 	/**
 	 *
 	 * @param messageID of the Game Message
-	 * @param playerX UserID of Player X
-	 * @param playerY UserID of Player Y
+	 * @param playerX   UserID of Player X
+	 * @param playerY   UserID of Player Y
 	 * @return The new created Game
 	 */
 	public static TicTacToe createGame(final TextChannel channel, final TicTacToePlayer playerX, final TicTacToePlayer playerY) {
 		final TicTacToe newGame = new TicTacToe(channel, playerX, playerY);
+		System.out.println(newGame);
 		games.add(newGame);
 		return newGame;
 	}
-	
+
 	public static void deleteGame(final TicTacToe game) {
 		games.remove(game);
 	}
-	
+
 	public static boolean isIngame(final String userID) {
 		for (final TicTacToe game : games)
 			if (game.getPlayerX().getId().equals(userID) || game.getPlayerY().getId().equals(userID))
 				return true;
 		return false;
 	}
-	
+
 	public static TicTacToe getGameByMessageID(final long messageID) {
 		return games.stream().filter(game -> game.getMessage().getIdLong() == messageID).findFirst().orElse(null);
 	}
