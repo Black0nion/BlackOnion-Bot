a0433360681dbcf03f86d350db8d1722128f1b38
Yusuf <yusufgamer222@gmail.com>
2022-04-07 17:01:57 +0100

    migrated the commands

--- src/main/java/com/github/black0nion/blackonionbot/commands/moderation/ClearWarnCommand.java
+++ src/main/java/com/github/black0nion/blackonionbot/commands/moderation/ClearWarnCommand.java
@@ -3,73 +3,73 @@
  */
 package com.github.black0nion.blackonionbot.commands.moderation;
 
+import com.github.black0nion.blackonionbot.commands.SlashCommand;
+import com.github.black0nion.blackonionbot.commands.SlashCommandEvent;
+import com.github.black0nion.blackonionbot.misc.Warn;
 import com.github.black0nion.blackonionbot.wrappers.jda.BlackGuild;
 import com.github.black0nion.blackonionbot.wrappers.jda.BlackMember;
 import com.github.black0nion.blackonionbot.wrappers.jda.BlackUser;
-import com.github.black0nion.blackonionbot.commands.TextCommand;
-import com.github.black0nion.blackonionbot.commands.CommandEvent;
-import com.github.black0nion.blackonionbot.misc.Warn;
-import com.github.black0nion.blackonionbot.utils.Utils;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @author _SIM_
  */
-public class ClearWarnCommand extends TextCommand {
+public class ClearWarnCommand extends SlashCommand {
+	private static final String USER = "user";
+	private static final String WARN_ID = "warnid";
 
 	public ClearWarnCommand() {
-		this.setCommand("clearwarn", "clearwarns").setSyntax("<@User> <warnid>").setRequiredArgumentCount(2).setRequiredPermissions(Permission.KICK_MEMBERS);
+		super(builder(Commands.slash("clearwarn", "Used to clear a warn.")
+				.addOption(OptionType.USER, USER, "The user to clear the warn from.", true)
+				.addOption(OptionType.NUMBER, WARN_ID, "The id of the warn to clear.", true))
+						.setRequiredPermissions(Permission.KICK_MEMBERS));
 	}
 
 	@Override
-	public void execute(final String[] args, final CommandEvent cmde, final MessageReceivedEvent e, final Message message, final BlackMember member, final BlackUser author, final BlackGuild guild, final TextChannel channel) {
-		final String user = args[1];
-		final BlackMember mentionedMember;
-		if (Utils.isLong(user)) {
-			mentionedMember = BlackMember.from(guild.retrieveMemberById(user).submit().join());
-			if (mentionedMember == null) {
-				cmde.error("usernotfound", "inputnumber");
+	public void execute(@NotNull SlashCommandEvent cmde, @NotNull SlashCommandInteractionEvent e, BlackMember member,
+			BlackUser author, @NotNull BlackGuild guild, TextChannel channel) {
+		var warnUserOption = e.getOption(USER);
+		var warnUser = Objects.requireNonNull(warnUserOption).getAsUser();
+		var warnMember = warnUserOption.getAsMember();
+		var warnId = e.getOption(WARN_ID, OptionMapping::getAsLong);
+
+		if (warnMember != null) {
+			var blackMember = BlackMember.from(guild.retrieveMemberById(warnMember.getId()).submit().join());
+			if (blackMember == null) {
+				cmde.send("memberisinvalid");
 				return;
+			}
+			final List<Warn> memberWarns = blackMember.getWarns();
+			for (final Warn warn : memberWarns) {
+				if (warn.date() == warnId) {
+					blackMember.deleteWarn(warn);
+					cmde.success("warndeleted", "warndeleted");
+					return;
+				} else {
+					cmde.send("invalidwarnid");
+				}
 			}
 		} else {
-			final List<Member> mentionedMembers = message.getMentionedMembers();
-			if (mentionedMembers.size() != 0) {
-				if (args[1].replace("!", "").equalsIgnoreCase(mentionedMembers.get(0).getAsMention())) {
-					mentionedMember = BlackMember.from(mentionedMembers.get(0));
-				} else {
-					cmde.sendPleaseUse();
+			var blackUser = BlackUser.from(e.getJDA().retrieveUserById(warnUser.getId()).submit().join());
+			final List<Warn> userWarns = blackUser.getWarns();
+			for (final Warn warn : userWarns) {
+				if (warn.date() == warnId) {
+					blackUser.deleteWarn(warn);
+					cmde.success("warndeleted", "warndeleted");
 					return;
+				} else {
+					cmde.send("invalidwarnid");
 				}
-			} else {
-				cmde.error("nousermentioned", "tagornameuser");
-				return;
 			}
-		}
-
-		try {
-			if (Utils.isLong(args[2])) {
-				final long warnId = Long.parseLong(args[2]);
-				assert mentionedMember != null;
-				final List<Warn> warns = mentionedMember.getWarns();
-				for (final Warn warn : warns) {
-					if (warn.date() == warnId) {
-						mentionedMember.deleteWarn(warn);
-						cmde.success("entrydeleted", "warndeleted");
-						return;
-					}
-				}
-				cmde.error("notfound", "warnnotfound");
-			} else {
-				cmde.sendPleaseUse();
-			}
-		} catch (final Exception ex) {
-			ex.printStackTrace();
 		}
 	}
 }
--- src/main/java/com/github/black0nion/blackonionbot/commands/moderation/SuggestCommand.java
+++ src/main/java/com/github/black0nion/blackonionbot/commands/moderation/SuggestCommand.java
@@ -3,42 +3,53 @@
  */
 package com.github.black0nion.blackonionbot.commands.moderation;
 
+import com.github.black0nion.blackonionbot.commands.SlashCommand;
+import com.github.black0nion.blackonionbot.commands.SlashCommandEvent;
+import com.github.black0nion.blackonionbot.utils.Utils;
 import com.github.black0nion.blackonionbot.wrappers.jda.BlackGuild;
 import com.github.black0nion.blackonionbot.wrappers.jda.BlackMember;
 import com.github.black0nion.blackonionbot.wrappers.jda.BlackUser;
-import com.github.black0nion.blackonionbot.commands.TextCommand;
-import com.github.black0nion.blackonionbot.commands.CommandEvent;
-import com.github.black0nion.blackonionbot.utils.Placeholder;
-import com.github.black0nion.blackonionbot.utils.Utils;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import org.jetbrains.annotations.NotNull;
 
-public class SuggestCommand extends TextCommand {
+public class SuggestCommand extends SlashCommand {
+	private static final String SUGGESTION = "suggestion";
 
 	public SuggestCommand() {
-		this.setCommand("suggest").setSyntax("<suggestion>").setRequiredArgumentCount(1);
+		super(builder(Commands.slash("suggest", "Used to of a suggestion").addOption(OptionType.STRING, SUGGESTION,
+				"The suggestion to send", true)));
 	}
 
 	@Override
-	public void execute(final String[] args, final CommandEvent cmde, final MessageReceivedEvent e, final Message message, final BlackMember member, final BlackUser author, final BlackGuild guild, final TextChannel channel) {
+	public void execute(@NotNull SlashCommandEvent cmde, @NotNull SlashCommandInteractionEvent e, BlackMember member,
+			BlackUser author, @NotNull BlackGuild guild, TextChannel channel) {
+		var suggestion = e.getOption(SUGGESTION, OptionMapping::getAsString);
 		final long suggestionsChannelId = guild.getSuggestionsChannel();
+
 		if (suggestionsChannelId == -1) {
-			cmde.error("suggestionsoff", "nosuggestionfeature");
+			cmde.send("invalidsuggestionschannel");
 		} else {
 			final TextChannel suggestionsChannel = guild.getTextChannelById(suggestionsChannelId);
 			if (suggestionsChannel == null) {
-				cmde.error("suggestionschannelnotfound", "pleaseresetsuggestionschannel");
-			} else if (!(guild.getSelfMember().hasPermission(suggestionsChannel, Permission.MESSAGE_SEND, Permission.MESSAGE_ADD_REACTION))) {
-				channel.sendMessageEmbeds(Utils.noRights(guild, guild.getSelfBlackMember().getBlackUser(), Permission.MESSAGE_SEND, Permission.MESSAGE_ADD_REACTION)).queue();
+				cmde.send("invalidsuggestionschannel");
+			} else if (!(guild.getSelfMember().hasPermission(suggestionsChannel, Permission.MESSAGE_SEND,
+					Permission.MESSAGE_ADD_REACTION))) {
+				e.replyEmbeds(Utils.noRights(guild, guild.getSelfBlackMember().getBlackUser(), Permission.MESSAGE_SEND,
+						Permission.MESSAGE_ADD_REACTION)).setEphemeral(true).queue();
 			} else {
 				// all good, we can send the suggestion
-				suggestionsChannel.sendMessageEmbeds(cmde.success().setTitle("suggestion").setDescription(String.join(" ", Utils.removeFirstArg(args))).build()).queue(msg -> {
-					msg.addReaction("U+1F44D").queue();
-					msg.addReaction("U+1F44E").queue();
-				});
-				cmde.success("suggestiongotsent", "suggestionisin", new Placeholder("channel", suggestionsChannel.getAsMention()));
+				suggestionsChannel.sendMessageEmbeds(
+						cmde.success().setTitle("suggestion").setDescription(String.join(" ", suggestion)).build())
+						.queue(msg -> {
+							msg.addReaction("U+1F44D").queue();
+							msg.addReaction("U+1F44E").queue();
+						});
+				cmde.send("suggestionsucess");
 			}
 		}
 	}
