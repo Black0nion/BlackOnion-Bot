52b12f0389b668496e13844905550068fdc58a0c
Yusuf Arfan Ismail <yusufgamer222@gmail.com>
2022-07-19 17:12:50 +0100

    update the token regex requirement

--- src/main/java/com/github/black0nion/blackonionbot/utils/config/Config.java
+++ src/main/java/com/github/black0nion/blackonionbot/utils/config/Config.java
@@ -17,7 +17,7 @@ import static com.github.black0nion.blackonionbot.utils.config.Flags.*;
 @SuppressWarnings("ConstantConditions")
 public class Config {
 
-	public static final String token = get("token", String.class, NonNull, matchesRegex("[MN][A-Za-z\\d]{23}\\.[\\w-]{6}\\.[\\w-]{27}"));
+	public static final String token = get("token", String.class, NonNull, matchesRegex("^[A-Za-z\\d]{24}.[\\w-]{6}.[\\w-]{26,40}$"));
 	public static final @Nonnull String prefix = get("prefix", String.class, defaultValue("*"), matchesRegex("\\w{1,10}"));
 	public static Activity.ActivityType activity_type = get("activity_type", Activity.ActivityType.class, defaultValue(Activity.ActivityType.PLAYING));
 	public static String activity_name = get("activity_name", String.class, defaultValue(prefix));
